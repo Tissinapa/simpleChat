d827d8915b2e3420bd32a5be6fa45185aad98536
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-04-09 12:03:54 +0300

    CODE: added second channel

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const host = "127.0.0.1"
 const port = "8124"
 
 let sockets = []
+let sockets2 = []
+
 
 const server = net.createServer(socket =>{
 
+    console.log("Client connected")
+    
     sockets.push(socket)
     socket.on("data", data =>{
         broadcast(data,socket)
@@ -25,15 +29,39 @@ server.listen(port,host, () => console.log("Server opened at Host: " + host + " 
 
 function broadcast(message, socketSent){
 
-    if (message.toString()==="quit"){
+    if (message.toString().match("/channel1") ){
+        console.log("Moveing to channel 1")
+        server.getConnections((err, clients)=>{
+            if(err){
+                console.log(err)
+            }
+            sockets.push(clients)
+            const index = sockets2.indexOf(socketSent)
+            sockets2.splice(index,1)
+
+        })
+
+    }else if (message.toString().match("/channel2")){
+        console.log("Moveing to channel 2")
+        server.getConnections((err, clients)=>{
+            if(err){
+                console.log(err)
+            }
+            sockets2.push(clients)
+            const index = sockets.indexOf(socketSent)
+            sockets.splice(index,1)
+
+        })
+
+    }else if(message.toString() ==="quit"){
         const index = sockets.indexOf(socketSent)
         sockets.splice(index,1)
     
     }else {
-        sockets.forEach(socket => {
-            if(socket !== socketSent){
+        sockets.forEach(client => {
+            if(client !== socketSent){
                 
-                socket.write(message)
+                client.write(message)
             }
             
         })
